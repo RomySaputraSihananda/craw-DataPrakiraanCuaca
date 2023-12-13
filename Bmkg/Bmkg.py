320d0c592426d2fb2eb9d1a6d912ab0d5d99e6ac
romys <romysaputrasihanandaa@gmail.com>
2023-12-13 23:01:04 +0700

    update

--- Bmkg/Bmkg.py
+++ Bmkg/Bmkg.py
@@ -1,10 +1,12 @@
 import requests
+import xmltodict
+
 from requests import Response
-import xmltodict 
-import xml.etree.ElementTree as ET
 from json import dumps
 from datetime import datetime
+
 from Bmkg.helpers import Kode_Parameter, Kode_Cuaca, Kode_Arah_Angin
+
 class Bmkg:
     def __init__(self) -> None:
         self.__BASE_URL: str = 'https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/'
@@ -23,8 +25,6 @@ class Bmkg:
         except:
             return Kode_Arah_Angin[text]
             
-
-
     def __filter_area(self, areas: list):
         for area in areas:
             self.__result['data'].append({
@@ -42,19 +42,18 @@ class Bmkg:
                                     for item in timerange['value']
                                 } if isinstance(timerange['value'], list) else {
                                     timerange['value']['@unit']: self.__str_2_number(timerange['value']['#text'] )
-                                } if timerange['value']['@unit'] != 'icon' else {
-                                    timerange['value']['@unit']: Kode_Cuaca[timerange['value']['#text']]
-                                }
+                                } if timerange['value']['@unit'] != 'icon' else Kode_Cuaca[timerange['value']['#text']]
                             } for timerange in parameter['timerange']
                         ]
                     } for parameter in area['parameter']
                 }
             })
 
-
     def execute(self, provinsi: str) -> str:
         url = self.__BASE_URL + f"DigitalForecast-{provinsi}.xml"
         res: Response = requests.get(url)
+
+        if(res.status_code != 200): return
 
         data = xmltodict.parse(res.content.decode('utf-8'))
 
@@ -71,5 +70,5 @@ if(__name__ == "__main__"):
     bmkg: Bmkg = Bmkg()
 
     with open('test_data.json', 'w') as file:
-        file.write(dumps(bmkg.execute('Aceh'), indent=2))
+        file.write(dumps(bmkg.execute('Banteng'), indent=2))
     # print(bmkg.execute('Aceh'))
